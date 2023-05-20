5dc215c94974191f0eb0daf0c9b4fecf77e601b3
individual-ism <individual-ism-code@proton.me>
2023-05-20 16:07:46 -0400

    Complete solution to Codecademy Whale Talk project

---
+++ build-a-text-adventure.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int fork1 {0};
+    int fork2 {0};
+    int fork3 {0};
+
+    std::cout << "The year is 2022.\nThe month February.\nThe day, the twenty-third.\n\nPresident of the United States Joseph Robinette Biden, Jr., issued warning to his Ukrainian counterpart, Volodymyr Zelensky, of an impending assault by the forces of the Russian Federation within the next forty-eight hours.\n";
+    std::cout << "";
+}
